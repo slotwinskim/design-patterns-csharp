39b7e0890912c2fe2074f6f7ce29540b68c70b50
Marcin Slotwinski <marcin.slotwinski@protonmail.com>
2021-09-17 20:05:30 +0100

    Added my implementation of Singleton

---
+++ Singleton.My/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Singleton.My
+{
+    class Program
+    {
+        static void Main()
+        {
+            var name = SingletonBase<Name>.Instance;
+            Console.WriteLine($"{name.First} -- {name.Last} -- {name.Birthday}");
+
+            var name1 = SingletonBase<Name>.Instance;
+            Console.WriteLine($"{name.First} -- {name.Last} -- {name.Birthday}");
+        }
+
+        public class Name
+        {
+            public string First => "first";
+            public string Last => "last";
+            public DateTime Birthday => DateTime.Now;
+        }
+
+        public sealed class SingletonBase<T>
+        {
+            private SingletonBase() { }
+
+            private static readonly Lazy<T> lazy = new();
+
+            public static T Instance => lazy.Value;
+        }
+    }
+}
