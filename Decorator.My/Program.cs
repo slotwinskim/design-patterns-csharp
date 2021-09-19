41c5eb236dea9fe19919d410dc95934b7aa53a47
Marcin Slotwinski <marcin.slotwinski@protonmail.com>
2021-09-19 20:53:37 +0100

    Added my EventAggregator and my Decorator

---
+++ Decorator.My/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Decorator.My
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var component = new ConcreteComponent();
+
+            var decorator = new ConcreteDecorator(component);
+            var decorator1 = new ConcreteDecorator(decorator);
+
+            var client = new Client();
+            var result = client.ClientCode(decorator1);
+            Console.WriteLine(result);
+        }
+
+        public abstract class Component
+        {
+            public abstract string DoStuff();
+        }
+
+        public class ConcreteComponent : Component
+        {
+            public override string DoStuff()
+            {
+                return "From ConcreteComponent";
+            }
+        }
+
+        public abstract class Decorator : Component
+        {
+            protected readonly Component component;
+
+            public Decorator(Component component)
+            {
+                this.component = component;
+            }
+
+            public override string DoStuff()
+            {
+                return component is not null
+                    ? component.DoStuff()
+                    : string.Empty;
+            }
+        }
+
+        public class ConcreteDecorator : Decorator
+        {
+            public ConcreteDecorator(Component component)
+                : base(component)
+            {
+            }
+
+            public override string DoStuff()
+            {
+                return $"From ConcreteDecorator '{base.DoStuff()}'";
+            }
+        }
+
+        public class Client
+        {
+            public string ClientCode(Component component)
+            {
+                return component.DoStuff();
+            }
+        }
+    }
+}
---
+++ Observer.EventAggregator.My/Program.cs
@@ -0,0 +1,90 @@
+﻿// https://www.martinfowler.com/eaaDev/EventAggregator.html
+// Channel events from multiple objects into a single object to simplify registration for clients.
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace Observer.EventAggregator.My
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var eventAggregator = new EventAggregator();
+            var doSomethingAfterPatientAdded = new DoSomethingWithAddingPatient();
+
+            eventAggregator.SubscribeTo(new PatientAdded(0), doSomethingAfterPatientAdded);
+
+            var addsPatient = new AddsPatient(eventAggregator);
+            addsPatient.DoStuff();
+        }
+
+        public class DoSomethingWithAddingPatient : ISubscriber
+        {
+            public void Notify(IEvent @event)
+            {
+                if (@event is PatientAdded patientAddEvent)
+                    Console.WriteLine($"Patient added with Id {patientAddEvent.Id}");
+            }
+        }
+
+        public class AddsPatient
+        {
+            private readonly EventAggregator eventAggregator;
+
+            public AddsPatient(EventAggregator eventAggregator)
+            {
+                this.eventAggregator = eventAggregator;
+            }
+
+            public void DoStuff()
+            {
+                eventAggregator.Publish(new PatientAdded(3));
+            }
+        }
+
+        public interface IEvent { }
+
+        public interface ISubscriber
+        {
+            public void Notify(IEvent @event);
+        }
+
+        public class PatientAdded : IEvent
+        {
+            public PatientAdded(int id)
+            {
+                Id = id;
+            }
+
+            public int Id { get; }
+        }
+
+        public class EventAggregator
+        {
+            private readonly ConcurrentDictionary<Type, List<ISubscriber>> events = new();
+            public void SubscribeTo(IEvent @event, ISubscriber subscruber)
+            {
+                if (events.TryGetValue(@event.GetType(), out var existing))
+                {
+                    existing.Add(subscruber);
+                    return;
+                }
+
+                events.TryAdd(@event.GetType(), new List<ISubscriber> { subscruber });
+            }
+
+            public void Publish(IEvent @event)
+            {
+                if(events.TryGetValue(@event.GetType(), out var publishers))
+                {
+                    foreach (var publisher in publishers)
+                    {
+                        publisher.Notify(@event);
+                    }
+                }
+            }
+        }
+    }
+}
